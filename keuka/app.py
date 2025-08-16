93aa7525ac79ef5b627ce83d758b7da700597839
Matt Reidy <matt@ereidy.com>
2025-08-16 17:04:38 -0400

    code update works again

--- keuka/app.py
+++ keuka/app.py
@@ -1,4 +1,4 @@
-# app.py v3
+# app.py v4
 # -----------------------------------------------------------------------------
 # Application factory for the Keuka Sensor web app.
 # - Creates a Flask app instance.
