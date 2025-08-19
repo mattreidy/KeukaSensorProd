c70cb6bfe06b949bbd11633c5b355410e38ad9e4
Matt Reidy <matt@ereidy.com>
2025-08-19 00:47:51 -0400

    v23

--- keuka/app.py
+++ keuka/app.py
@@ -1,4 +1,4 @@
-# app.py v22
+# app.py v23
 # ----------------------------------------------------------------------------
 # Application factory for the Keuka Sensor web app.
 # - Creates a Flask app instance.
